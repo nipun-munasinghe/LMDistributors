98ad4e9a02e0477e03f2aa35e88a48664aac0fcf
nipun-munasinghe <nipunlakmal884@gmail.com>
2024-11-15 19:30:30 +0530

    Start to design the home page

---
+++ header.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>L. M. Distributors</title>
+
+    <!-- link the stylesheet -->
+    <link rel="stylesheet" href="./css/header.css">
+
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+
+    <!-- set the logo into title -->
+    <link rel="icon" type="image/x-icon" href="images/favicon.ico">
+</head>
+<body>
+    <header class="header">
+        <div class="header-container">
+            <div class="logo">
+                <a id="lm" href="./index.php"><img src="./images/LMDistributors-logo2-01.png" alt="L. M. Distributors Logo"></a>
+                <h1><a id="lm" href="./index.php">L. M. Distributors</a></h1>
+            </div>
+
+            <nav class="nav-links">
+                <a href="./index.php">Home</a>
+                <a href="./products.php">Products</a>
+                <a href="./about.php">About Us</a>
+                <a href="./contact.php">Contact Us</a>
+            </nav>
+
+            <div class="mode-switch">
+                <label for="theme-toggle">
+                    <span class="fas fa-sun" id="sun-icon"></span>
+                    <input type="checkbox" id="theme-toggle">
+                    <span class="fas fa-moon" id="moon-icon"></span>
+                </label>
+            </div>
+        </div>
+
+        <div class="loginBtns">
+            <button id="hLogin" onclick="window.location.href='./login.php'">Sign In</button>
+            <button id="hLSignup" onclick="window.location.href='./register.php'">Sign Up</button>
+        </div>
+    </header>
+    
+<script src="./header.js"></script>
+</body>
+</html>
