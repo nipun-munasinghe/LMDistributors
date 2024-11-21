3a2c41e976fabef25c5ef724ef9c646b69d7e1dc
nipun-munasinghe <nipunlakmal884@gmail.com>
2024-11-21 23:26:12 +0530

    Updated customer profile
    Updated customer profile landing page

--- customer.php
+++ customer.php
@@ -36,9 +36,48 @@
     <!-- Main Content -->
     <div class="main-content">
             <i class="fas fa-bars" id="open-sidebar"></i>
-            <h1>Welcome to Your Profile</h1>
+            <h1>Welcome FirstName!</h1>
 
-        
+            <section class="profileDetails">
+                <div>
+                    <img src="images/default-profile.png" alt="Profile Picture" class="profile-pic" id="profile-pic-preview">
+                </div>
+                <div class="nameType">
+                    <h3>First LastName</h3>
+                    <p>Customer</p>
+                </div>
+            </section>
+
+            <br>
+
+            <div class="slideshow-container">
+                <!-- Slide 1 -->
+                <div class="slide fade">
+                    <img src="./images/slide1.jpg" alt="Slide 1">
+                </div>
+                <!-- Slide 2 -->
+                <div class="slide fade">
+                    <img src="./images/slide2.jpg" alt="Slide 2">
+                </div>
+                <!-- Slide 3 -->
+                <div class="slide fade">
+                    <img src="./images/slide3.jpg" alt="Slide 3">
+                </div>
+                <!-- Slide 4 -->
+                <div class="slide fade">
+                    <img src="./images/slide4.jpg" alt="Slide 4">
+                </div>
+            </div>
+
+            <div class="cards">
+                <div class="products">
+                    <a href="./products.php"><i class="fas fa-box-open"></i><br>Products</a>
+                </div>
+
+                <div class="settings">
+                    <a href="./accSettings.php"><i class="fa-solid fa-wrench"></i><br>Account Settings</a>
+                </div>
+            </div>
     </div>
 
     <!-- Footer -->
