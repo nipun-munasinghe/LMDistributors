b4e391563b061e17238ea454fb545a51b6e7d00d
nipun-munasinghe <nipunlakmal884@gmail.com>
2024-12-12 22:42:52 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+
+//database configs
+$server = "localhost";
+$username = "root";
+$password = "";
+$database = "lmdistributors";
+
+//create connection
+$conn = new mysqli($server, $username, $password, $database);
+
+//check connection
+
+if($conn -> connect_error) {
+    die("Connection failed:".$conn -> connect_error);
+}
+
+echo "Successfully connected";
+
+?>
