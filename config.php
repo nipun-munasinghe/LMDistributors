768c6775d3acb4f9205412fdfa5af174cc1d98a9
nipun-munasinghe <nipunlakmal884@gmail.com>
2024-12-14 22:03:01 +0530

    Update config.php

--- config.php
+++ config.php
@@ -10,7 +10,6 @@ $database = "lmdistributors";
 $conn = new mysqli($server, $username, $password, $database);
 
 //check connection
-
 if($conn -> connect_error) {
     die("Connection failed:".$conn -> connect_error);
 }
