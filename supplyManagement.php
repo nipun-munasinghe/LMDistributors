89b4d2a7fc16110dad29e19ebb9ca534028d3b49
nipun-munasinghe <nipunlakmal884@gmail.com>
2025-01-04 20:06:38 +0530

    Update supplyManagement.php
    Updated with basic things like greeting and sidebar.

--- supplyManagement.php
+++ supplyManagement.php
@@ -12,10 +12,18 @@
         exit();
     }
     else {
-        
-    }
+        $dob = $_SESSION['user_dob'];
+        $today = date('m-d');
+        $birthday = date('m-d', strtotime($dob));
 
-    
+        // check user's birthday
+        if($birthday == $today) {
+            $greeting = "Happy birthday ".htmlspecialchars($_SESSION['user_fName'])."!";
+        }
+        else {
+            $greeting = "Welcome ".htmlspecialchars($_SESSION['user_fName'])."!";
+        }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -42,24 +50,7 @@
     <div class="container">
         <!-- Hidden Sidebar -->
         <aside id="sidebar" class="sidebar">
-            <div class="sidebar-header">
-                <h2>Admin Dashboard</h2>
-                <i class="fas fa-times" id="close-sidebar" title="Close Dashboard"></i>
-            </div>
-
-            <ul class="sidebar-menu">
-                <li><a href="./admin.php" title="Your Profile"><i class="fa-solid fa-user"></i> My Profile</a></li>
-                <li><a href="./manageManagers.php" title="Managers Management"><i class="fas fa-user-tie"></i> Manage Managers</a></li>
-                <li><a href="./manageProducts.php" title="Products Management"><i class="fa-solid fa-store"></i> Manage Products</a></li>
-                <li><a href="./manageOrders.php" title="Orders Management"><i class="fa-solid fa-cart-shopping"></i> Manage Orders</a></li>
-                <li><a href="./todayPrice.php" title="Buyers & Sellers price list"><i class="fa-solid fa-money-bill-1-wave"></i> Today's Price List</a></li>
-                <li><a href="./buyersManagement.php" title="Buyers Management"><i class="fa-solid fa-handshake"></i> Manage Buyers</a></li>
-                <li><a href="./supplyManagement.php" title="Suppliers Management"><i class="fa-solid fa-business-time"></i> Manage Suppliers</a></li>
-                <li><a href="./manageMessages.php" title="Messages Management"><i class="fa-solid fa-comment"></i> Manage Messages</a></li>
-                <li><a href="./viewIncome.php" title="View Analytics"><i class="fas fa-chart-line"></i> View Analytics</a></li>
-                <li><a href="./accSettings.php" title="Edit profile & Change password"><i class="fas fa-cog"></i> Settings</a></li>
-                <li><a href="./logout.php" title="Logout"><i class="fas fa-sign-out-alt"></i> Logout</a></li>
-            </ul>
+            <?php include'sidebar.php';?>
         </aside>
 
         <!-- Main Content -->
@@ -67,7 +58,7 @@
             <!-- Topbar -->
             <div class="topbar">
                 <i class="fas fa-bars" id="toggle-sidebar" title="Open Dashboard"></i>
-                <h1>Welcome AdminName!</h1>
+                <h1><?php echo $greeting; ?></h1>
             </div>
 
             <h2 class="displaySuppliersH2">Suppliers' Management Details</h2>
@@ -101,71 +92,12 @@
                                 <td class="Phone">0788753442</td>
                                 <td class="comments">null</td>
                                 <td class="tStatus">Rejected</td>
-                                <td class="tAction"><a href="#"><i class="fa-solid fa-thumbs-up" title="Accept"></i></a> | 
-                                                    <a href="#"><i class="fa-solid fa-thumbs-down" title="Reject"></i></a> | 
-                                                    <a href="#"><i class="fa-solid fa-trash-can" title="Delete"></i></a></td>
+                                <td class="tAction">
+                                    <a href="#"><i class="fa-solid fa-thumbs-up" title="Accept"></i></a> | 
+                                    <a href="#"><i class="fa-solid fa-thumbs-down" title="Reject"></i></a> | 
+                                    <a href="#"><i class="fa-solid fa-trash-can" title="Delete"></i></a>
+                                </td>
                             </tr>
-                            <tr>
-                                <td class="supplyID">2</td>
-                                <td class="supplierName">Aberathne</td>
-                                <td class="reqDate">09/12/2024</td>
-                                <td class="supplyDate">15/12/2024</td>
-                                <td class="supplyQuantity">2500</td>
-                                <td class="ourPrice">125.50</td>
-                                <td class="theirPrice">125.00</td>
-                                <td class="Phone">0788759466</td>
-                                <td class="comments">Lower price</td>
-                                <td class="tStatus">Accepted</td>
-                                <td class="tAction"><a href="#"><i class="fa-solid fa-thumbs-up" title="Accept"></i></a> | 
-                                                    <a href="#"><i class="fa-solid fa-thumbs-down" title="Reject"></i></a> | 
-                                                    <a href="#"><i class="fa-solid fa-trash-can" title="Delete"></i></a></td>
-                            </tr>
-                            <tr>
-                                <td class="supplyID">3</td>
-                                <td class="supplierName">Jayindi Coco Lanka</td>
-                                <td class="reqDate">09/12/2024</td>
-                                <td class="supplyDate">11/12/2024</td>
-                                <td class="supplyQuantity">10000</td>
-                                <td class="ourPrice">125.50</td>
-                                <td class="theirPrice">125.50</td>
-                                <td class="Phone">0766753442</td>
-                                <td class="comments">Ten thousand kilo grams</td>
-                                <td class="tStatus">null</td>
-                                <td class="tAction"><a href="#"><i class="fa-solid fa-thumbs-up" title="Accept"></i></a> | 
-                                                    <a href="#"><i class="fa-solid fa-thumbs-down" title="Reject"></i></a> | 
-                                                    <a href="#"><i class="fa-solid fa-trash-can" title="Delete"></i></a></td>
-                            </tr>
-                            <tr>
-                                <td class="supplyID">4</td>
-                                <td class="supplierName">Kamal Rathnasiri</td>
-                                <td class="reqDate">10/12/2024</td>
-                                <td class="supplyDate">20/12/2024</td>
-                                <td class="supplyQuantity">3600</td>
-                                <td class="ourPrice">125.50</td>
-                                <td class="theirPrice">124.50</td>
-                                <td class="Phone">0788555442</td>
-                                <td class="comments">null</td>
-                                <td class="tStatus">Accepted</td>
-                                <td class="tAction"><a href="#"><i class="fa-solid fa-thumbs-up" title="Accept"></i></a> | 
-                                                    <a href="#"><i class="fa-solid fa-thumbs-down" title="Reject"></i></a> | 
-                                                    <a href="#"><i class="fa-solid fa-trash-can" title="Delete"></i></a></td>
-                            </tr>
-                            <tr>
-                                <td class="supplyID">5</td>
-                                <td class="supplierName">Samagi Products</td>
-                                <td class="reqDate">18/12/2024</td>
-                                <td class="supplyDate">30/12/2024</td>
-                                <td class="supplyQuantity">1000</td>
-                                <td class="ourPrice">125.50</td>
-                                <td class="theirPrice">125.50</td>
-                                <td class="Phone">0788753442</td>
-                                <td class="comments">null</td>
-                                <td class="tStatus">null</td>
-                                <td class="tAction"><a href="#"><i class="fa-solid fa-thumbs-up" title="Accept"></i></a> | 
-                                                    <a href="#"><i class="fa-solid fa-thumbs-down" title="Reject"></i></a> | 
-                                                    <a href="#"><i class="fa-solid fa-trash-can" title="Delete"></i></a></td>
-                            </tr>
-                            
                         </table>
                     </div>
                 </div>
