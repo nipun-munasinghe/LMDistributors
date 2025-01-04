b23d4fbf3ab366064418375f76f94c50d5fac788
nipun-munasinghe <nipunlakmal884@gmail.com>
2025-01-04 20:27:13 +0530

    Update logout.php
    Add logout time function

--- logout.php
+++ logout.php
@@ -1,6 +1,21 @@
 <?php
     session_start();
 
+    // Set the correct time zone
+    date_default_timezone_set('Asia/Colombo');
+
+    include_once 'config.php';
+
+    // Update last logout time in the database
+    if (isset($_SESSION['user_id'])) {
+        $userId = $_SESSION['user_id'];
+        $logoutTime = date('Y-m-d H:i:s');
+        
+        // Update query to store logout time in the database
+        $query = "UPDATE user_info SET last_logout = '$logoutTime' WHERE userID = '$userId'";
+        mysqli_query($conn, $query) or die("Error updating logout time: " . mysqli_error($conn));
+    }
+
     // Unset all of the session variables
     $_SESSION = array();
 
