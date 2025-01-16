4bc11a4f3b064efbe51d633538f6d8c3aaea9c62
nipun-munasinghe <nipunlakmal884@gmail.com>
2025-01-16 13:36:17 +0530

    Update manager.php
    Enhanced Security

--- manager.php
+++ manager.php
@@ -6,12 +6,7 @@
     include_once 'config.php';
 
     // check user is logged or not
-    if(!isset($_SESSION['user_fName'])) {
-        // user is not logged in, redirect to login page
-        header('Location: login.php');
-        exit();
-    }
-    else {
+    if(isset($_SESSION['user_fName']) && $_SESSION['user_type'] == 'manager') {
         $dob = $_SESSION['user_dob'];
         $today = date('m-d');
         $birthday = date('m-d', strtotime($dob));
@@ -24,6 +19,11 @@
             // welcome user with welcome message
             $greeting = 'Welcome '.htmlspecialchars($_SESSION['user_fName'].'!');
         }
+    }
+    else {
+        // user is not logged in, redirect to login page
+        header('Location: login.php');
+        exit();
     }
 ?>
 
